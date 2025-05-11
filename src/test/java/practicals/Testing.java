fc66c323fe3affd182f769665ac52b217811a540
mohanjuly1988 <mohanjuly1988@gmail.com>
2025-05-11 19:51:20 +0530

    Mgr commit1

---
+++ src/test/java/practicals/Testing.java
@@ -0,0 +1,13 @@
+package practicals;
+
+public class Testing {
+
+	public static void main(String[] args) 
+	{
+	 System.out.println("Code by Manager");
+	 //SDET A must code first
+	 //SDET B must code second
+
+	}
+
+}
